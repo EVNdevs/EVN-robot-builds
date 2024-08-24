6ce5a0fd442711a0001487ba82c19a974fba8498
HTY2003 <randumbperson@gmail.com>
2024-08-24 16:15:13 +0800

    Update entrybot example code

--- entrybot/entrybot.ino
+++ entrybot/entrybot.ino
@@ -4,43 +4,44 @@
 #define CENTRE_MOTOR_PORT   3
 #define RIGHT_MOTOR_PORT    4
 
-EVNAlpha board(BUTTON_TOGGLE, LED_LINK, MOTORS_LINK);
+EVNAlpha board;
 EVNMotor motor_left(LEFT_MOTOR_PORT, EV3_LARGE);
 EVNMotor motor_right(RIGHT_MOTOR_PORT, EV3_LARGE);
 EVNMotor motor_centre(CENTRE_MOTOR_PORT, EV3_MED);
+
+void setup1()
+{
+	//intialize motors on 2nd core
+	motor_left.begin();
+	motor_right.begin();
+	motor_centre.begin();
+}
 
 void setup()
 {
 	board.begin();  //initialize board at start of void setup()
 }
 
-void setup1()
-{
-	//intialize motors on 2nd core for better performance
-	//but only using one core works too
-
-	motor_left.begin();
-	motor_right.begin();
-	motor_centre.begin();
-}
-
 void loop()
 {
 	//this is very basic code but you could code any kind of movement here!
 
-	//on startup, the motors are disabled (so the user can upload code in peace)
-	//pressing the user-defined button will toggle the motors on and off (can be disabled if needed)
-
-	//move all motors
-	motor_left.runSpeed(-50);
-	motor_right.runSpeed(50);
-	motor_centre.runSpeed(100);
+	//start all motors
+	motor_left.runSpeed(-360);
+	motor_right.runSpeed(360);
+	motor_centre.runSpeed(180);
 	delay(3000);
 
 	//reverse all motors
-	motor_left.runSpeed(50);
-	motor_left.runSpeed(-50);
-	motor_centre.runSpeed();
+	motor_left.runSpeed(360);
+	motor_right.runSpeed(-360);
+	motor_centre.runSpeed(-180);
+	delay(3000);
+
+	//stop all motors
+	motor_left.stop();
+	motor_right.stop();
+	motor_centre.stop();
 	delay(3000);
 
 }
